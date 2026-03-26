3ba6cfcd89127d43fb6639206dba413711419a40
Dmitry D <divin.dm1try@mail.ru>
2026-03-26 17:05:53 +0300

    [ADDED] Favorites storage for managing images

--- GalleryApp/Services/FavoritesStorage.swift
+++ GalleryApp/Services/FavoritesStorage.swift
@@ -9,9 +9,34 @@ import Foundation
 import Combine
 
 protocol IFavoritesStorage {
+    func getAllFavoritesIds() -> Set<String>
+    func toggleFavorite(id: String)
+    func isFavorite(id: String) -> Bool
     
 }
 
 final class FavoritesStorage: IFavoritesStorage {
+    
+    private let userDefaults = UserDefaults.standard
+    private let favoritesKey = AppConstants.Strings.favoritesKey
+    
+    func getAllFavoritesIds() -> Set<String> {
+        let ids = userDefaults.stringArray(forKey: favoritesKey) ?? []
+        return Set(ids)
+    }
+    
+    func toggleFavorite(id: String) {
+        var favorites = getAllFavoritesIds()
+        if favorites.contains(id) {
+            favorites.remove(id)
+        } else {
+            favorites.insert(id)
+        }
+        userDefaults.set(Array(favorites), forKey: favoritesKey)
+    }
+    
+    func isFavorite(id: String) -> Bool {
+        return getAllFavoritesIds().contains(id)
+    }
     
 }
--- GalleryApp/Services/NetworkService.swift
+++ GalleryApp/Services/NetworkService.swift
@@ -10,13 +10,15 @@ import Combine
 
 protocol INetworkService{
     
+    func fetchImages(page: Int, perPage: Int) -> AnyPublisher<[Images], Error>
+    
 }
 
 final class NetworkService: INetworkService {
     
     private let accessKey = AppConstants.unsplashAccessKey
     
-    func fetchImages(page: Int, perPage: Int = 30) -> AnyPublisher<[Images], Error> {
+    func fetchImages(page: Int, perPage: Int = AppConstants.Numbers.numberOfFetchingImages) -> AnyPublisher<[Images], Error> {
         let urlString = "https://api.unsplash.com/photos?page=\(page)&per_page=\(perPage)&order_by=latest"
         
         guard let url = URL(string: urlString) else {
@@ -32,6 +34,6 @@ final class NetworkService: INetworkService {
             .mapError { $0 as Error}
             .eraseToAnyPublisher()
     }
-        
-    }
     
+}
+
