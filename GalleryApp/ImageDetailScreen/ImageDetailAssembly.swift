7db5da751838b0dd786225dc85365c8729b271a7
Dmitry D <divin.dm1try@mail.ru>
2026-04-01 13:34:03 +0300

    Reapply "[ADDED] UI elements, simple logic"
    This reverts commit 76ec5d2ad11441f183c5881b320aa720f8e209c6.

--- GalleryApp/ImageDetailScreen/ImageDetailAssembly.swift
+++ GalleryApp/ImageDetailScreen/ImageDetailAssembly.swift
@@ -10,8 +10,13 @@ import UIKit
 final class ImageDetailAssembly {
     static func assemble(images: [Images],
                          initialIndex: Int,
-                         coordinator: AppCoordinator) -> UIViewController {
-        let viewModel = ImageDetailViewModel(images: images, initialIndex: initialIndex)
+                         coordinator: AppCoordinator,
+                         favoritesStorage: IFavoritesStorage) -> UIViewController {
+        
+        let viewModel = ImageDetailViewModel(images: images,
+                                             initialIndex: initialIndex,
+                                             favoritesStorage: favoritesStorage)
+        
         let viewController = ImageDetailViewController(viewModel: viewModel)
         viewModel.coordinator = coordinator
         return viewController
